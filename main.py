eb554402d648b6f75ee689b7a94d4c24de4b9cf2
Edwin Wagha <edwin.wagha20@gmail.com>
2026-01-26 02:51:08 +0300

    update

--- main.py
+++ main.py
@@ -34,13 +34,46 @@ class RubiksMosaicGenerator:
 
         # Find the index (0-5) of the smallest distance and return that color
         return self.palette[np.argmin(distances)] 
+    
+
+    def generate_mosaic(self):
+        try:
+            original_image = Image.open(self.image_path)
+        except FileNotFoundError:
+            print(f"File not found: {self.image_path}")
+            return None
+        
+        # 1 cube wide = 3 stickers // WIDTH
+        target_width = self.cubes_wide * 3
 
 
+        #height to match the aspect ratio
+        aspect_ratio = original_image.width / original_image.height
+        target_height = int(target_width / aspect_ratio)
 
+        # We need the height to be perfectly divisible by 3
+        # Because we cannot have a row of partial cubes at the end
+        # We strip off the remainder to ensure we have full 3x3 blocks
 
-rubiks = RubiksMosaicGenerator('image.jpg')
+        remainder = target_height % 3
+        target_height = target_height - remainder
+
+        print(f"Grid Size: {target_width}x{target_height} stickers.")
+        print(f"Total Physical Cubes: {self.cubes_wide} x {target_height // 3}")
+
+        # downscale the Image(pixelated)
+        # shrink the photo so that 1 pixel = 1 sticker.
+        # example - If we want a 1x1 Cube mosaic, this creates a 3x3 pixel image.
+        image_small = original_image.resize((target_width, target_height), resample=Image.BILINEAR)
+
+        image_array = np.array(image_small)
+
+        
+
+rubiks = RubiksMosaicGenerator('image.png')
 
 #print(rubiks.get_closet_color((0, 0, 0))) return the closest color to the pixel
 
+print(rubiks.generate_mosaic())
 
 
