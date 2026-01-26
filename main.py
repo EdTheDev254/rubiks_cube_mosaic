6ce63bc7434c7b4b260188d48579d97894d11b06
Edwin Wagha <edwin.wagha20@gmail.com>
2026-01-26 03:18:54 +0300

    update - grid/stickers logic added and the saving logic

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ ORANGE = (255, 165, 0)
 YELLOW = (255, 255, 0)
 
 # grid line separator
-GRAY = (128, 128, 128)
+CUBE_BORDER = (128, 128, 128)
+STICKER_BORDER = (0, 0, 0)
 
 
 class RubiksMosaicGenerator:
@@ -26,7 +27,7 @@ class RubiksMosaicGenerator:
         #print(self.palette.shape) shape is (6,3)
 
     # perform a euclidean difference to find the single closest color to the pixel
-    def get_closet_color(self, pixel):
+    def get_closest_color(self, pixel): #typo haha
         
         # square to remove negative numbers and sum them horizontally = results in,
         # 6 numbers rep the total error of each of the Rubiks cube color
@@ -36,7 +37,7 @@ class RubiksMosaicGenerator:
         return self.palette[np.argmin(distances)] 
     
 
-    def generate_mosaic(self):
+    def generate_mosaic(self, output_scale=10):
         try:
             original_image = Image.open(self.image_path)
         except FileNotFoundError:
@@ -68,12 +69,72 @@ class RubiksMosaicGenerator:
 
         image_array = np.array(image_small)
 
-        
+        # create a blank canvas (matrix) of zeros with the exact same shape as our image....
+        mosaic_array = np.zeros_like(image_array)
 
-rubiks = RubiksMosaicGenerator('image.png')
+
+        for row in range(target_height):# row 
+            for col in range(target_width): # column
+                
+                pixel = image_array[row, col] # get pixel color
+                
+                closest = self.get_closest_color(pixel) # find closest color
+                
+                # We write the closest color into the specific slot on the blank canvas.
+                mosaic_array[row, col] = closest 
+
+        # .astype('uint8') ensures the numbers are standard (0-255) image colors.
+        mosaic_img = Image.fromarray(mosaic_array.astype('uint8'))
+
+        # upscale
+        final_width = target_width * output_scale
+        final_height = target_height * output_scale
+        
+        # nearest for pixelated/hard edges
+        final_img = mosaic_img.resize((final_width, final_height), resample=Image.NEAREST)
+
+        # We prepare the image for drawing.
+
+        draw = ImageDraw.Draw(final_img)
+        
+        # Added these for aesthetics purposes :D
+        # These lines separate every single individual color patch.
+        for x in range(0, final_width, output_scale):
+            draw.line([(x, 0), (x, final_height)], fill=STICKER_BORDER, width=1)
+            
+        for y in range(0, final_height, output_scale):
+            draw.line([(0, y), (final_width, y)], fill=STICKER_BORDER, width=1)
+
+        # separte the cubes
+        # These lines separate the physical 3x3 cubes.
+        cube_pixel_size = 3 * output_scale
+        
+        for x in range(0, final_width, cube_pixel_size):
+            draw.line([(x, 0), (x, final_height)], fill=CUBE_BORDER, width=3)
+            
+        for y in range(0, final_height, cube_pixel_size):
+            draw.line([(0, y), (final_width, y)], fill=CUBE_BORDER, width=3)
+
+        return final_img
+
+if __name__ == "__main__":
+    input_file = 'image.png'
+    
+    print(f"Processing {input_file}........")
+
+    generator = RubiksMosaicGenerator(input_file, cubes_wide=100) # cubes_wide is the number of cubes
+    
+    result = generator.generate_mosaic(output_scale=10) # output_scale is the pixel size
+    
+    if result:
+        result.save('output.png')
+        print("Success! ...Saved as 'rubiks_mosaic_output.png'")
+        result.show()
 
 #print(rubiks.get_closet_color((0, 0, 0))) return the closest color to the pixel
 
-print(rubiks.generate_mosaic())
+#print(rubiks.generate_mosaic())
+
+
 
 
